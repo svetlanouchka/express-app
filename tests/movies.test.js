36d90277dd922678b758b30dc57aa52770c02305
Svetlana Preux-Nigay <sveta.nigai@gmail.com>
2025-01-09 10:15:13 +0100

    added tests movie id

--- tests/movies.test.js
+++ tests/movies.test.js
@@ -3,9 +3,26 @@ const app = require("../app")
 
 describe("GET /api/movies", () => {
     it("should return all movies", async () => {
-        const response = await request(app).get("api/movies");
+        const response = await request(app).get("/api/movies");
 
         expect(response.headers["content-type"]).toMatch(/json/);
         expect(response.status).toEqual(200);
     });
+});
+
+describe("GET /api/movies/:id", () => {
+    it("should return movie id 1", async () => {
+        const response = await request(app).get("/api/movies/1");
+
+        expect(response.headers["content-type"]).toMatch(/json/);
+        expect(response.status).toEqual(200);
+    });
+});
+
+describe("GET /api/movies/:id", () => {
+    it("should return movie id 0", async () => {
+        const response = await request(app).get("/api/movies/0");
+
+        expect(response.status).toEqual(400);
+    })
 });
